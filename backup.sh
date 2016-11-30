2ee0fe40156239b15fd807a715e08621baa1183c
Mark Stansberry [CIE] <mark.stansberry@emc.com>
2016-11-30 16:07:29 -0500

    updates

--- backup.sh
+++ backup.sh
@@ -5,12 +5,16 @@ base="/share/Containers"
 
 # Export config dirs for containers
 if [ ! -d $base/backup ]; then
-    mkdir $base/backup/
+    mkdir $base/backup
 fi
 
-for name in $(ls -ld $base/* | grep "^d" | grep -v "backup\|station" | awk '{print $9}' | cut -d/ -f4); do
+cd $base/media-vpn
+tar zcvf $base/backup/media-vpn.tgz config/
+
+for name in $(ls -ld $base/* | grep "^d" | grep -v "backup\|station\|media-vpn" | awk '{print $9}' | cut -d/ -f4); do
   docker stop $name
-  chown -R 911:911 $base/$name/config
-  tar zcvf $base/backup/$name.tgz $base/$name/config
+  cd $base/$name
+  chown -R 911:911 config/
+  tar zcvf $base/backup/$name.tgz config/
   docker start $name
 done
--- build.sh
+++ build.sh
@@ -1,30 +1,40 @@
 #! /bin/bash
 
-# Define base path for all containers
+# Define base path for all
 base="/share/Containers"
 media="/share/Multimedia"
 dl="/share/Download"
 
-# Create muximux container
+# Create media-vpn container (openvpn/sock proxy)
+# Ports: proxy=1080 rutorrent=9080:80 jackett=9117
 docker run -d \
-  --name=muximux \
-  -p 8081:80 \
-  -v $base/muximux/config:/config \
-  linuxserver/muximux
+  --restart=always \
+  --name=media-vpn \
+  -h media-vpn \
+  --device=/dev/net/tun \
+  --cap-add=NET_ADMIN \
+  -p 1080:1080 \
+  -p 9080:80 \
+  -p 9117:9117 \
+  -v $base/media-vpn/config/openvpn.ovpn:/ovpn.conf:ro \
+  -v $base/media-vpn/config/openvpn.cred:/openvpn.cred:ro \
+  mook/openvpn-client-socks
 
 # Create plex container
 docker run -d \
-  --name=plex \
+  --name plex \
+  -h plex \
   --net=host \
   -e VERSION=latest \
   -v $base/plex/config:/config \
   -v $media/transcode:/transcode \
-  -v $media/music:/data/music \
-  -v $media/video/:/data/video \
+  -v $media:/data \
   linuxserver/plex
 
+# Create plexpy container
 docker run -d \
-  --name=plexpy \
+  --name plexpy \
+  -h plexpy \
   -p 8181:8181 \
   -v $base/plexpy/config:/config \
   -v $base/plex/config/Library/Application\ Support/Plex\ Media\ Server/Logs:/logs:ro \
@@ -33,60 +43,62 @@ docker run -d \
 # Create nzbget container
 docker run -d \
   --name nzbget \
+  -h nzbget \
   -p 6789:6789 \
   -v $base/nzbget/config:/config \
-  -v $dl:/downloads/ \
-  -v $media:/output \
+  -v $dl:/downloads \
+  -v $media:/media \
   linuxserver/nzbget
 
-# Create transmission container
-docker run --privileged -d \
-  --name=transmission \
-  -v $base/transmission/config:/config \
-  -v $dl/torrent:/downloads \
-  -v $dl/torrent:/watch \
-  -p 9091:9091 \
-  -p 51413:51413 \
-  -p 51413:51413/udp \
-  transmission-openvpn
+# Create rutorrent container
+docker run -d \
+  --name rutorrent \
+  --net=container:media-vpn \
+  -v $base/rutorrent/config:/config \
+  -v $dl:/downloads \
+  -v $media:/media \
+  linuxserver/rutorrent
 
 # Create hydra container
 docker run -d \
-  --name=hydra \
-  --link=nzbget \
-  -p 5075:5075 \
+  --name hydra \
+  -h hydra \
   -v $base/hydra/config:/config \
-  -v $dl:/downloads \
+  -p 5075:5075 \
   linuxserver/hydra
 
 # Create jacket tcontainer
 docker run -d \
-  --name=jackett \
+  --name jackett \
+  --net=container:media-vpn \
   -v $base/jackett/config:/config \
-  -v $dl/torrent:/downloads \
-  -p 9117:9117 \
+  -v $dl:/downloads \
   linuxserver/jackett
 
 # Create couchpotato container
 docker run -d \
-  --name=couchpotato \
-  --link=jackett \
-  --link=nzbget \
-  --link=transmission \
+  --name couchpotato \
+  -h couchpotato \
   -p 5050:5050 \
   -v $base/couchpotato/config:/config \
   -v $dl:/downloads \
-  -v $media/video/Movies:/movies \
+  -v $media:/media \
   linuxserver/couchpotato
 
 # Create sonarr container
 docker run -d \
-  --name=sonarr \
-  --link=jackett \
-  --link=nzbget \
-  --link=transmission \
+  --name sonarr \
+  -h sonarr \
   -p 8989:8989 \
   -v $base/sonarr/config:/config \
-  -v $media/video/TV\ Shows:/tv \
   -v $dl:/downloads \
+  -v $media/video/TV\ Shows/:/tv \
   linuxserver/sonarr
+
+# Create muximux container
+docker run -d \
+  --name muximux \
+  -h muximux \
+  -p 8081:80 \
+  -v $base/muximux/config:/config \
+  linuxserver/muximux
