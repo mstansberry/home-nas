56ca99bfeb219fc8d65e75341a46898b49ee7da4
Mark Stansberry [CIE] <mark.stansberry@emc.com>
2016-12-05 13:00:43 -0500

    updates

--- backup.sh
+++ backup.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+# Usage: backup.sh all
+
+# Read input
+name="$1"
+if [[ $name == "" ]]; then
+  name="all"
+fi
 
 # Define base path for all containers
 base="/share/Containers"
@@ -11,10 +18,18 @@ fi
 cd $base/media-vpn
 tar zcvf $base/backup/media-vpn.tgz config/
 
-for name in $(ls -ld $base/* | grep "^d" | grep -v "backup\|station\|media-vpn" | awk '{print $9}' | cut -d/ -f4); do
+if [[ $name == "all" ]]; then
+  for cont in $(ls -ld $base/* | grep "^d" | grep -v "backup\|station\|media-vpn" | awk '{print $9}' | cut -d/ -f4); do
+    docker stop $cont
+    cd $base/$cont
+    chown -R 911:911 config/
+    tar zcvf $base/backup/$cont.tgz config/
+    docker start $cont
+  done
+else
   docker stop $name
   cd $base/$name
   chown -R 911:911 config/
   tar zcvf $base/backup/$name.tgz config/
   docker start $name
-done
+fi
--- build.sh
+++ build.sh
@@ -1,104 +1,137 @@
 #! /bin/bash
+# Usage: build.sh all
+
+# Read input
+name="$1"
+if [[ $name == "" ]]; then
+  name="all"
+fi
 
 # Define base path for all
 base="/share/Containers"
 media="/share/Multimedia"
 dl="/share/Download"
 
-# Create media-vpn container (openvpn/sock proxy)
-# Ports: proxy=1080 rutorrent=9080:80 jackett=9117
-docker run -d \
-  --restart=always \
-  --name=media-vpn \
-  -h media-vpn \
-  --device=/dev/net/tun \
-  --cap-add=NET_ADMIN \
-  -p 1080:1080 \
-  -p 9080:80 \
-  -p 9117:9117 \
-  -v $base/media-vpn/config/openvpn.ovpn:/ovpn.conf:ro \
-  -v $base/media-vpn/config/openvpn.cred:/openvpn.cred:ro \
-  mook/openvpn-client-socks
+# Create media-vpn container (openvpn)
+# built from http://github.com/linuxserver/docker-vpn
+# Ports: deluge=8112 jackett=9117
+if [[ $name == "media-vpn" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name=media-vpn \
+    -h media-vpn \
+    --device=/dev/net/tun \
+    --cap-add=NET_ADMIN \
+    -p 8112:8112 \
+    -p 9117:9117 \
+    -e FORCEVPN=true \
+    -v $base/media-vpn/config:/config \
+    openvpn
+fi
 
 # Create plex container
-docker run -d \
-  --name plex \
-  -h plex \
-  --net=host \
-  -e VERSION=latest \
-  -v $base/plex/config:/config \
-  -v $media/transcode:/transcode \
-  -v $media:/data \
-  linuxserver/plex
+if [[ $name == "plex" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name plex \
+    -h plex \
+    --net=host \
+    -e VERSION=latest \
+    -v $base/plex/config:/config \
+    -v $media/transcode:/transcode \
+    -v $media:/data \
+    linuxserver/plex
+fi
 
 # Create plexpy container
-docker run -d \
+if [[ $name == "plexpy" || $name == "all" ]]; then
+  docker run -d \
   --name plexpy \
-  -h plexpy \
-  -p 8181:8181 \
-  -v $base/plexpy/config:/config \
-  -v $base/plex/config/Library/Application\ Support/Plex\ Media\ Server/Logs:/logs:ro \
-  linuxserver/plexpy
+    -h plexpy \
+    -p 8181:8181 \
+    -v $base/plexpy/config:/config \
+    -v $base/plex/config/Library/Application\ Support/Plex\ Media\ Server/Logs:/logs:ro \
+    linuxserver/plexpy
+fi
 
 # Create nzbget container
-docker run -d \
-  --name nzbget \
-  -h nzbget \
-  -p 6789:6789 \
-  -v $base/nzbget/config:/config \
-  -v $dl:/downloads \
-  -v $media:/media \
-  linuxserver/nzbget
+if [[ $name == "nzbget" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name nzbget \
+    -h nzbget \
+    -p 6789:6789 \
+    -v $base/nzbget/config:/config \
+    -v $dl:/downloads \
+    -v $media:/media \
+    linuxserver/nzbget
+fi
 
-# Create rutorrent container
-docker run -d \
-  --name rutorrent \
-  --net=container:media-vpn \
-  -v $base/rutorrent/config:/config \
-  -v $dl:/downloads \
-  -v $media:/media \
-  linuxserver/rutorrent
+# Create deluge container
+if [[ $name == "deluge" || $name == "all" ]]; then
+  docker run -d \
+    --name deluge \
+    --net=container:media-vpn \
+    -v $dl:/downloads \
+    -v $base/deluge/config:/config \
+    linuxserver/deluge
+fi
 
 # Create hydra container
-docker run -d \
-  --name hydra \
-  -h hydra \
-  -v $base/hydra/config:/config \
-  -p 5075:5075 \
-  linuxserver/hydra
+if [[ $name == "hydra" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name hydra \
+    -h hydra \
+    -v $base/hydra/config:/config \
+    -p 5075:5075 \
+    linuxserver/hydra
+fi
+
 
 # Create jacket tcontainer
-docker run -d \
-  --name jackett \
-  --net=container:media-vpn \
-  -v $base/jackett/config:/config \
-  -v $dl:/downloads \
-  linuxserver/jackett
+if [[ $name == "jackett" || $name == "all" ]]; then
+  docker run -d \
+    --name jackett \
+    --net=container:media-vpn \
+    -v $base/jackett/config:/config \
+    -v $dl:/downloads \
+    linuxserver/jackett
+fi
 
 # Create couchpotato container
-docker run -d \
-  --name couchpotato \
-  -h couchpotato \
-  -p 5050:5050 \
-  -v $base/couchpotato/config:/config \
-  -v $dl:/downloads \
-  -v $media:/media \
-  linuxserver/couchpotato
+if [[ $name == "couchpotato" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name couchpotato \
+    -h couchpotato \
+    -p 5050:5050 \
+    -v $base/couchpotato/config:/config \
+    -v $dl:/downloads \
+    -v $media:/media \
+    linuxserver/couchpotato
+fi
 
 # Create sonarr container
-docker run -d \
-  --name sonarr \
-  -h sonarr \
-  -p 8989:8989 \
-  -v $base/sonarr/config:/config \
-  -v $dl:/downloads \
-  -v $media/video/TV\ Shows/:/tv \
-  linuxserver/sonarr
+if [[ $name == "sonarr" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name sonarr \
+    -h sonarr \
+    -p 8989:8989 \
+    -v $base/sonarr/config:/config \
+    -v $dl:/downloads \
+    -v $media/video/TV\ Shows/:/tv \
+    linuxserver/sonarr
+fi
 
 # Create muximux container
-docker run -d \
-  --name muximux \
-  -h muximux \
-  -p 8081:80 \
-  -v $base/muximux/config:/config \
-  linuxserver/muximux
+if [[ $name == "muximux" || $name == "all" ]]; then
+  docker run -d \
+    --restart=always \
+    --name muximux \
+    -h muximux \
+    -p 8081:80 \
+    -v $base/muximux/config:/config \
+    linuxserver/muximux
+fi
