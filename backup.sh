a8e6ae7421af23a0c138a92a97acbdb936f112f9
Mark Stansberry [CIE] <mark.stansberry@emc.com>
2016-12-09 18:59:23 -0500

    encrypted backups

--- backup.sh
+++ backup.sh
@@ -8,6 +8,8 @@ if [[ $name == "" ]]; then
   name="all"
 fi
 
+gpg_key="$2"
+
 # Define base path for all containers
 base="/share/Containers"
 
@@ -17,20 +19,23 @@ if [ ! -d $base/backup ]; then
 fi
 
 cd $base/media-vpn
-tar zcvf $base/backup/media-vpn.tgz config/
+rm -f $base/backup/media-vpn.tgz.gpg
+tar zcvpf - config/ | gpg --passphrase "$gpg_key" --symmetric --cipher-algo aes256 -o $base/backup/media-vpn.tgz.gpg
 
 if [[ $name == "all" ]]; then
-  for cont in $(ls -ld $base/* | grep "^d" | grep -v "backup\|station\|media-vpn" | awk '{print $9}' | cut -d/ -f4); do
+  for cont in $(ls -ld $base/* | grep "^d" | grep -v "backup\|station\|media-vpn\|Recycle" | awk '{print $9}' | cut -d/ -f4); do
     docker stop $cont
     cd $base/$cont
-    chown -R 911:911 config/
-    tar zcvf $base/backup/$cont.tgz config/
+    chown -R media:everyone config/
+    rm -f $base/backup/$cont.tgz.gpg
+    tar zcvpf - config/ | gpg --passphrase "$gpg_key" --symmetric --cipher-algo aes256 -o $base/backup/$cont.tgz.gpg
     docker start $cont
   done
 else
   docker stop $name
   cd $base/$name
-  chown -R 911:911 config/
-  tar zcvf $base/backup/$name.tgz config/
+  chown -R media:everyone config/
+  rm -f $base/backup/$name.tgz.gpg
+  tar zcvpf - config/ | gpg --passphrase "$gpg_key" --symmetric --cipher-algo aes256 -o $base/backup/$name.tgz.gpg
   docker start $name
 fi
--- restore.sh
+++ restore.sh
@@ -7,19 +7,19 @@ if [[ $name == "" ]]; then
   name="all"
 fi
 
+gpg_key="$2"
+
 # Define base path for all containers
 base="/share/Containers"
-media="/share/Multimedia"
-dl="/share/Download"
 
 # Restore configs from backups
 if [[ $name == "all" ]]; then
-  for cont in $(ls $base/backup/*.tgz | cut -d/ -f5 | cut -d. -f1); do
+  for cont in $(ls $base/backup/*.gpg | cut -d/ -f5 | cut -d. -f1); do
     docker rm -f $cont
     if [ ! -d $base/$cont ]; then
       mkdir $base/$cont $base/$cont/config
     fi
-    tar zxvf $base/backup/$cont.tgz -C $base/$cont/
+    echo "$gpg_key" | gpg --passphrase-fd 0 --batch -d $base/backup/$cont.tgz.gpg | tar zxv -C $base/$cont/
   done
   . $base/build.sh all
 else
@@ -29,7 +29,7 @@ else
   fi
 
   if [ ! -f $base/backup/$name.tgz ]; then
-    tar zxvf $base/backup/$name.tgz -C $base/$name/
+    echo "$gpg_key" | gpg --passphrase-fd 0 --batch -d $base/backup/$name.tgz.gpg | tar zxv -C $base/$name/
   fi
   . $base/build.sh $name
 fi
